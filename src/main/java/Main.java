309c0474e29afa6be2d4ac25363ff8109b12bc46
Eleanor Foltan <edfoltan@wpi.edu>
2019-03-14 12:50:19 -0400

    Added my name

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Cooper Was Here");
         System.out.println("Have some merge issues");
+        System.out.println("Eleanor Foltan");
     }
 }
